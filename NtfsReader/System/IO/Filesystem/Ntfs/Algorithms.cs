8d6e34ef710a83e07995670d82c089642bb78f23
zerk666 <zerk666@d4f7f664-bbcd-447f-a897-531b711e4f25>
2008-10-13 17:23:30 +0000

    v1.0

---
+++ NtfsReader/System/IO/Filesystem/Ntfs/Algorithms.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace System.IO.Filesystem.Ntfs
+{
+    public static class Algorithms
+    {
+        public static IDictionary<UInt32, List<INode>> AggregateByFragments(IEnumerable<INode> nodes, UInt32 minimumFragments)
+        {
+            Dictionary<UInt32, List<INode>> fragmentsAggregate = new Dictionary<UInt32, List<INode>>();
+
+            foreach (INode node in nodes)
+            {
+                IList<IStream> streams = node.Streams;
+                if (streams == null || streams.Count == 0)
+                    continue;
+
+                IList<IFragment> fragments = streams[0].Fragments;
+                if (fragments == null)
+                    continue;
+
+                UInt32 fragmentCount = (UInt32)fragments.Count;
+
+                if (fragmentCount < minimumFragments)
+                    continue;
+
+                List<INode> nodeList;
+                fragmentsAggregate.TryGetValue(fragmentCount, out nodeList);
+
+                if (nodeList == null)
+                {
+                    nodeList = new List<INode>();
+                    fragmentsAggregate[fragmentCount] = nodeList;
+                }
+
+                nodeList.Add(node);
+            }
+
+            return fragmentsAggregate;
+        }
+
+        public static IDictionary<UInt64, List<INode>> AggregateBySize(IEnumerable<INode> nodes, UInt64 minimumSize)
+        {
+            Dictionary<UInt64, List<INode>> sizeAggregate = new Dictionary<ulong, List<INode>>();
+
+            foreach (INode node in nodes)
+            {
+                if ((node.Attributes & Attributes.Directory) != 0 || node.Size < minimumSize)
+                    continue;
+
+                List<INode> nodeList;
+                sizeAggregate.TryGetValue(node.Size, out nodeList);
+
+                if (nodeList == null)
+                {
+                    nodeList = new List<INode>();
+                    sizeAggregate[node.Size] = nodeList;
+                }
+
+                nodeList.Add(node);
+            }
+
+            return sizeAggregate;
+        }
+    }
+}
